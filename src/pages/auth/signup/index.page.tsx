31fd5eccd6f3fce9ef39763ad7434180f9084e5b
Adam Mellor <melloradam97@gmail.com>
2023-08-29 19:36:29 +0100

    use page extension

--- src/pages/auth/signup/index.page.tsx
+++ src/pages/auth/signup/index.page.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router"
 import Layout from "src/core/layouts/Layout"
-import { SignupForm } from "@/features/auth/components/SignupForm"
+import { SignupForm } from "@/pages/auth/signup/SignupForm"
 import { BlitzPage, Routes } from "@blitzjs/next"
 
 const SignupPage: BlitzPage = () => {
