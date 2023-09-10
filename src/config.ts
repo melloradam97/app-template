734c874e4c0a987c1e59c071356259f619650dc2
Adam Mellor <melloradam97@gmail.com>
2023-09-10 15:29:53 +0100

    fix emails in prod

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,5 @@
 export const isDev = process.env.NODE_ENV === "development"
-const PROD_URL = process.env.PROD_URL
 const DEV_URL = "http://localhost:3000"
 
-export const URL_ORIGIN = isDev ? DEV_URL : PROD_URL
+export const URL_ORIGIN = isDev ? DEV_URL : "https://template-production-e14c.up.railway.app/"
 export const APP_NAME = "APP NAME"
