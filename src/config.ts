a1edb3b0f76d9295a61920ea3c2dc07cb822fab7
Adam Mellor <melloradam97@gmail.com>
2023-09-11 14:19:58 +0100

    add config to gitignore

--- src/config.ts
+++
@@ -1,5 +0,0 @@
-export const isDev = process.env.NODE_ENV === "development"
-const DEV_URL = "http://localhost:3000"
-
-export const URL_ORIGIN = isDev ? DEV_URL : "https://template-production-e14c.up.railway.app/"
-export const APP_NAME = "APP NAME"
